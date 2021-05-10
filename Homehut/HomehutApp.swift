7c0f03812e1ba5442c2d5d40f198082347fdd803
Jacob Fink <jacob.fink@allscripts.com>
2021-05-10 14:47:23 -0400

    Initial Commit

---
+++ Homehut/HomehutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomehutApp.swift
+//  Homehut
+//
+//  Created by Jacob Fink on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HomehutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
